e382939398a1b8ee0de3df39d290b4d058d89b12
Temp <Temp@DESKTOP-R3GRJ71>
2021-12-03 09:58:31 -0800

    Add project files.

---
+++ AOC2021-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AOC2021_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = File.ReadAllLines("C:\\Users\\Temp\\Documents\\AOC2021-3.txt");
+            string gamma = "", epsilon = "";
+            double counter;
+            int numG, numE;
+
+            for (int i = 0; i < inputs[0].Length; i++)
+            {
+                counter = 0;
+
+                for (int x = 0; x < inputs.Length; x++)
+                {
+                    counter += double.Parse(inputs[x].Substring(i, 1));
+                }
+
+                if (counter / inputs.Length > 0.5)
+                {
+                    gamma += "1";
+                    epsilon += "0";
+                }
+                else
+                {
+                    gamma += "0";
+                    epsilon += "1";
+                }
+
+            }
+
+            numG = Convert.ToInt32(gamma, 2);
+            numE = Convert.ToInt32(epsilon, 2);
+
+            Console.WriteLine(gamma);
+            Console.WriteLine(numG);
+            Console.WriteLine(epsilon);
+            Console.WriteLine(numE);
+            Console.ReadKey();
+        }
+    }
+}
